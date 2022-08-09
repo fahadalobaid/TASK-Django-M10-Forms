9fdf16dbdc6fa620f20dd810338009b2b466033f
fahad alobaid <fahad.n.b.alobaid@gmail.com>
2022-08-09 20:16:46 +0300

    end creat

--- stores/views.py
+++ stores/views.py
@@ -1,8 +1,7 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render ,redirect
+from .forms import StoreItemForm
 from stores import models
-
 
 def get_store_items(request: HttpRequest) -> HttpResponse:
     store_items: list[models.StoreItem] = list(models.StoreItem.objects.all())
@@ -10,3 +9,18 @@ def get_store_items(request: HttpRequest) -> HttpResponse:
         "store_items": store_items,
     }
     return render(request, "store_item_list.html", context)
+
+
+def create_store_item(request,):
+    form = StoreItemForm()
+    if request.method == "POST":
+        form = StoreItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('store-item-list')
+
+    context = {
+        "form" : form,
+
+    }
+    return render(request,'create_store_item.html',context)
